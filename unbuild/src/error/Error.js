4365b3fd44825bde6e8f188459f6f8c38bec4ff2
Kirill Suranov <78699588+Kss03@users.noreply.github.com>
2023-04-11 19:50:44 +0200

    Delete unbuild directory

--- unbuild/src/error/Error.js
+++
@@ -1,7 +0,0 @@
-import { Spinner } from "react-bootstrap"
-
-export default function Error() {
-  return (
-    <Spinner className="position-fixed start-50 top-50" animation="grow" variant="danger" />
-  )
-}
--- unbuild/src/loading/Loading.js
+++
@@ -1,7 +0,0 @@
-import { Spinner } from "react-bootstrap"
-
-export default function Loading() {
-  return (
-    <Spinner className="position-fixed start-50 top-50" animation="border" variant="secondary"/>
-  )
-}
